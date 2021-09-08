86a6383bc043ccb0a38e4cb97922d02a21251409
IndominusByte <nyomanpradipta120@gmail.com>
2021-09-08 14:00:29 +0800

    basic auth

---
+++ basic-auth/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+if [ -z $username ]; then
+  echo >&2 "username must be set"
+  exit 1
+fi
+
+if [ -z $password ]; then
+  echo >&2 "password must be set"
+  exit 1
+fi
+
+htpasswd -bBc /etc/nginx/.htpasswd $username $password
+
+nginx -g 'daemon off;'
